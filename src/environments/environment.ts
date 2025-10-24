a09843ec54b0a8e1a238c5d2958301406dc04e9f
Nkosi2000 <nkosiphila901@gmail.com>
2025-10-24 16:48:18 +0200

    Use new restricted API key for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-      apiKey: "${AIzaSyBwrEA338T24LP99o2fDXZ59cYwejP0Wto}",
-      authDomain: "${project-e4d2b.firebaseapp.com}",
-      databaseURL: "${https://project-e4d2b-default-rtdb.firebaseio.com}",
-      projectId: "${project-e4d2b}",
-      storageBucket: "${project-e4d2b.firebasestorage.app}",
-      messagingSenderId: "${433495637743}",
-      appId: "${1:433495637743:web:42d57369801121a8e0a51a}"
+      apiKey: "AIzaSyAx3gqVUKw3WI_X69o_NNoE8N-QukLGrb0",
+      authDomain: "project-e4d2b.firebaseapp.com",
+      databaseURL: "https://project-e4d2b-default-rtdb.firebaseio.com",
+      projectId: "project-e4d2b",
+      storageBucket: "project-e4d2b.firebasestorage.app",
+      messagingSenderId: "433495637743",
+      appId: "1:433495637743:web:42d57369801121a8e0a51a"
     }
 };
