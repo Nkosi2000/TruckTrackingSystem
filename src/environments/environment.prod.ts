110a4efa5bab61061bc36259eb39d95194a23354
Nkosi2000 <nkosiphila901@gmail.com>
2025-10-24 16:06:26 +0200

    Add environment files with placeholders

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "${AIzaSyBwrEA338T24LP99o2fDXZ59cYwejP0Wto}",
+      authDomain: "${project-e4d2b.firebaseapp.com}",
+      databaseURL: "${https://project-e4d2b-default-rtdb.firebaseio.com}",
+      projectId: "${project-e4d2b}",
+      storageBucket: "${project-e4d2b.firebasestorage.app}",
+      messagingSenderId: "${433495637743}",
+      appId: "${1:433495637743:web:42d57369801121a8e0a51a}"
+    }
+};
